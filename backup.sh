f9ce09ecea6d66617fbfaf26233987936a5bfdab
Marc Urben <aegnor@mittelerde.ch>
2014-02-20 15:31:58 +0100

    added the option to remove files older than X days in backupcleanup

--- backup.sh
+++ backup.sh
@@ -4,110 +4,110 @@
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
 function rdiffbackup {
-    mountbackup
-    FOLDERNAME="/$BACKUPDIR/oxirdiffbackup/$(hostname)/$2/"
+	mountbackup
+	FOLDERNAME="/$BACKUPDIR/oxirdiffbackup/$(hostname)/$2/"
 
-    CHOWNO=$(chmod 777 $FOLDERNAME)
+	CHOWNO=$(chmod 777 $FOLDERNAME)
 
-    MKDIRO=$(mkdir -p $FOLDERNAME 2>&1)
+	MKDIRO=$(mkdir -p $FOLDERNAME 2>&1)
 
-    PARAMETER=""
-    for DIR in ${EXCLUDED_DIR}; do
+	PARAMETER=""
+	for DIR in ${EXCLUDED_DIR}; do
 	PARAMETER="$PARAMETER--exclude $DIR "
-    done
+	done
 
 
-    RDIFF1O=$($(which rdiff-backup) $PARAMETER $1 $FOLDERNAME 2>/dev/null)
-    RDIFF2O=$($(which rdiff-backup) --force --remove-older-than $3 $1 2>/dev/null)
+	RDIFF1O=$($(which rdiff-backup) $PARAMETER $1 $FOLDERNAME 2>/dev/null)
+	RDIFF2O=$($(which rdiff-backup) --force --remove-older-than $3 $1 2>/dev/null)
 
-    SIZEF=$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/$2)
-    SIZEH=$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/)
-    SIZET=$(du -sh $BACKUPDIR/oxirdiffbackup/)
+	SIZEF=$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/$2)
+	SIZEH=$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/)
+	SIZET=$(du -sh $BACKUPDIR/oxirdiffbackup/)
 
-    NEWLISTING=$(ls -lha $BACKUPDIR/oxirdiffbackup/$(hostname)/$2/)
-    MOUNT=$(mount | grep $BACKUPDIR 2>&1)
+	NEWLISTING=$(ls -lha $BACKUPDIR/oxirdiffbackup/$(hostname)/$2/)
+	MOUNT=$(mount | grep $BACKUPDIR 2>&1)
 
-    umountbackup
+	umountbackup
 
-    if [ -n "$MOUNT" ]; then
-        MOUNT="mount:\t$MOUNT\n"
-    fi
+	if [ -n "$MOUNT" ]; then
+		MOUNT="mount:\t$MOUNT\n"
+	fi
 
-    if [ -n "$MOUNTO" ]; then
-        MOUNTO="mount:\t$MOUNTO\n"
-    fi
+	if [ -n "$MOUNTO" ]; then
+		MOUNTO="mount:\t$MOUNTO\n"
+	fi
 
-    if [ -n "$UMOUNTO" ]; then
-        UMOUNTO="mount:\t$UMOUNTO\n"
-    fi
+	if [ -n "$UMOUNTO" ]; then
+		UMOUNTO="mount:\t$UMOUNTO\n"
+	fi
 
-    if [ -n "$RDIFF1O" ]; then
-        TARO="rdiff-backup:\t$RDIFF1O\n"
-    fi
+	if [ -n "$RDIFF1O" ]; then
+		TARO="rdiff-backup:\t$RDIFF1O\n"
+	fi
 
-    if [ -n "$MKDIRO" ]; then
-        MKDIRO="mount:\t$MKDIRO\n"
-    fi
+	if [ -n "$MKDIRO" ]; then
+		MKDIRO="mount:\t$MKDIRO\n"
+	fi
 
-    SIZE="size total:\t$SIZET\nsize host:\t$SIZEH\nsize fabric:\t$SIZEF"
+	SIZE="size total:\t$SIZET\nsize host:\t$SIZEH\nsize fabric:\t$SIZEF"
 
-    if [ $DEBUG -gt 0 ]; then
-        notifyadmin "$(hostname) $2 rdiff-backup" "-- FILES IN $FOLDERNAME --\n$NEWLISTING\n\n-- SIZE INFOS --\n$SIZE\n\n-- DEBUG INFOS --\n$MOUNT$RDIFF1O\n$RDIFF2O\n$MOUNTO$UMOUNTO$MKDIRO\n$CHOWNO"
-    fi
+	if [ $DEBUG -gt 0 ]; then
+		notifyadmin "$(hostname) $2 rdiff-backup" "-- FILES IN $FOLDERNAME --\n$NEWLISTING\n\n-- SIZE INFOS --\n$SIZE\n\n-- DEBUG INFOS --\n$MOUNT$RDIFF1O\n$RDIFF2O\n$MOUNTO$UMOUNTO$MKDIRO\n$CHOWNO"
+	fi
 }
 
 
 function backup {
-    mountbackup
-    FILENAME="/$BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m)/$(basename $1).$TIMESTAMP.tar.bz2"
-    
-    MKDIRO=$(mkdir -p $BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m) 2>&1)
-    TARO=$(/bin/tar -cjf $FILENAME $1 2>/dev/null)
-    
-    SIZEF=$(du -sh $BACKUPDIR/oxibackup/$(hostname)/$2)
-    SIZEH=$(du -sh $BACKUPDIR/oxibackup/$(hostname)/)
-    SIZET=$(du -sh $BACKUPDIR/oxibackup/)
-    
-    NEWLISTING=$(ls -lha $BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m))
-    MOUNT=$(mount | grep $BACKUPDIR 2>&1)
-    
-    umountbackup
-    
-    if [ -n "$MOUNT" ]; then
+	mountbackup
+	FILENAME="/$BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m)/$(basename $1).$TIMESTAMP.tar.bz2"
+	
+	MKDIRO=$(mkdir -p $BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m) 2>&1)
+	TARO=$(/bin/tar -cjf $FILENAME $1 2>/dev/null)
+	
+	SIZEF=$(du -sh $BACKUPDIR/oxibackup/$(hostname)/$2)
+	SIZEH=$(du -sh $BACKUPDIR/oxibackup/$(hostname)/)
+	SIZET=$(du -sh $BACKUPDIR/oxibackup/)
+	
+	NEWLISTING=$(ls -lha $BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m))
+	MOUNT=$(mount | grep $BACKUPDIR 2>&1)
+	
+	umountbackup
+	
+	if [ -n "$MOUNT" ]; then
 	MOUNT="mount:\t$MOUNT\n"
-    fi
+	fi
 
-    if [ -n "$MOUNTO" ]; then
+	if [ -n "$MOUNTO" ]; then
 	MOUNTO="mount:\t$MOUNTO\n"
-    fi
-    
-    if [ -n "$UMOUNTO" ]; then
+	fi
+	
+	if [ -n "$UMOUNTO" ]; then
 	UMOUNTO="mount:\t$UMOUNTO\n"
-    fi
+	fi
 
-    if [ -n "$TARO" ]; then
+	if [ -n "$TARO" ]; then
 	TARO="tar:\t$TARO\n"
-    fi
-    
-    if [ -n "$MKDIRO" ]; then
+	fi
+	
+	if [ -n "$MKDIRO" ]; then
 	MKDIRO="mount:\t$MKDIRO\n"
-    fi
-    
-    SIZE="size total:\t$SIZET\nsize host:\t$SIZEH\nsize fabric:\t$SIZEF"
-    
-    if [ $DEBUG -gt 0 ]; then    
+	fi
+	
+	SIZE="size total:\t$SIZET\nsize host:\t$SIZEH\nsize fabric:\t$SIZEF"
+	
+	if [ $DEBUG -gt 0 ]; then    
 	notifyadmin "$(hostname) $2 backup" "-- FILES IN $BACKUPDIR/oxibackup/$(hostname)/$2/$(date +%Y%m) --\n$NEWLISTING\n\n-- SIZE INFOS --\n$SIZE\n\n-- DEBUG INFOS --\n$MOUNT$TARO$MOUNTO$UMOUNTO$MKDIRO" 
-    fi
+	fi
 }
 
 function rsyncbackup {
-    while $(test -f /var/run/oxiscripts-rsyncbackup.pid); do sleep 10; done
-    echo $$ > /var/run/oxiscripts-rsyncbackup.pid
+	while $(test -f /var/run/oxiscripts-rsyncbackup.pid); do sleep 10; done
+	echo $$ > /var/run/oxiscripts-rsyncbackup.pid
 
-    PARAMETER=""
-    for DIR in ${EXCLUDED_DIR}; do
-        PARAMETER="$PARAMETER--exclude $DIR "
-    done
+	PARAMETER=""
+	for DIR in ${EXCLUDED_DIR}; do
+		PARAMETER="$PARAMETER--exclude $DIR "
+	done
 
 	if [ -z "$RSYNCPASSWORD" ]; then
 		RSYNCO=$($(which rsync) -avh $3 ${PARAMETER} $1 $2)
@@ -119,64 +119,77 @@ function rsyncbackup {
 		rm /etc/oxiscripts/rsyncpw-$$.tmp
 	fi
 
-    if [ $DEBUG -gt 0 ]; then
-        notifyadmin "$(hostname) $2 rsync-backup" "-- DEBUG INFOS --\n$RSYNCO"
-    fi
-    rm /var/run/oxiscripts-rsyncbackup.pid
+	if [ $DEBUG -gt 0 ]; then
+		notifyadmin "$(hostname) $2 rsync-backup" "-- DEBUG INFOS --\n$RSYNCO"
+	fi
+	rm /var/run/oxiscripts-rsyncbackup.pid
 }
 
 
 function backupinfo {
-    mountbackup
-    
-    SIZET="size total:\t$(du -sh $BACKUPDIR/oxibackup/)"
-    SIZEH="size host:\t$(du -sh $BACKUPDIR/oxibackup/$(hostname)/)"
+	mountbackup
+	
+	SIZET="size total:\t$(du -sh $BACKUPDIR/oxibackup/)"
+	SIZEH="size host:\t$(du -sh $BACKUPDIR/oxibackup/$(hostname)/)"
 
 	SIZEF="size of oxibackup:\n"
-    for FABRIC in $(ls $BACKUPDIR/oxibackup/$(hostname)/); do
+	for FABRIC in $(ls $BACKUPDIR/oxibackup/$(hostname)/); do
 	SIZEF="$SIZEF$(du -sh $BACKUPDIR/oxibackup/$(hostname)/$FABRIC)\n"
 	for MONTH in $(ls $BACKUPDIR/oxibackup/$(hostname)/$FABRIC); do
-	    SIZEF="$SIZEF$(du -sh $BACKUPDIR/oxibackup/$(hostname)/$FABRIC/$MONTH | awk '{print $1}')\t\t$MONTH\n"
+		SIZEF="$SIZEF$(du -sh $BACKUPDIR/oxibackup/$(hostname)/$FABRIC/$MONTH | awk '{print $1}')\t\t$MONTH\n"
 	done
 	SIZEF="$SIZEF\n\n"
-    done
+	done
 
 	if [ -d $BACKUPDIR/oxirdiffbackup/$(hostname)/ ]; then
 		SIZEF="$SIZEFsize of oxirdiffbackup:\n"
-	    for FABRIC in $(ls $BACKUPDIR/oxirdiffbackup/$(hostname)/); do
+		for FABRIC in $(ls $BACKUPDIR/oxirdiffbackup/$(hostname)/); do
 		SIZEF="$SIZEF$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/$FABRIC)\n"
 		for FOLDER in $(ls $BACKUPDIR/oxirdiffbackup/$(hostname)/$FABRIC); do
-		    SIZEF="$SIZEF$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/$FABRIC/$FOLDER | awk '{print $1}')\t\t$FOLDER\n"
+			SIZEF="$SIZEF$(du -sh $BACKUPDIR/oxirdiffbackup/$(hostname)/$FABRIC/$FOLDER | awk '{print $1}')\t\t$FOLDER\n"
 		done
 		SIZEF="$SIZEF\n"
-	    done
+		done
 	fi
 
 
-    notifyadmin "$(hostname) backup info (uptime: $(uptime))" "-- $(hostname) backup usage --\n\n$SIZET\n$SIZEH\n\n-- fabrics --\n$SIZEF"
+	notifyadmin "$(hostname) backup info (uptime: $(uptime))" "-- $(hostname) backup usage --\n\n$SIZET\n$SIZEH\n\n-- fabrics --\n$SIZEF"
 
-    umountbackup
+	umountbackup
 }
 
 function backupcleanup {
-    mountbackup
-    
-    if [ -n $(which fdupes) ]; then
-        SIZEBEFORE=$(du -sh $BACKUPDIR/oxibackup/$(hostname))
-	COUNT=0
-        for FILE in $(fdupes -r -f -q $BACKUPDIR/oxibackup/$(hostname)); do
-		COUNT=$(($COUNT+1))
-	    rm $FILE
-        done
-	SIZEAFTER=$(du -sh $BACKUPDIR/oxibackup/$(hostname))
+	mountbackup
+	
+	if [ -n $(which fdupes) ]; then
+		SIZEBEFORE=$(du -sh $BACKUPDIR/oxibackup/$(hostname))
+		COUNT=0
+		for FILE in $(fdupes -r -f -q $BACKUPDIR/oxibackup/$(hostname)); do
+			COUNT=$(($COUNT+1))
+			rm $FILE
+		done
 
-        if [ $COUNT -gt 0 ]; then
-	    notifyadmin "$(hostname) backup cleanup" "-- $(hostname) backup cleanup --\n\nfiles cleaned:\t$COUNT\nsize before:\t$SIZEBEFORE\nsize after:\t$SIZEAFTER"
-        fi
-    else
+		if [[ $1 =~ ^-?[0-9]+$ ]]: then
+			find $BACKUPDIR/oxibackup/$(hostname) -mtime +$1 -type f exec rm {} \;
+
+			find "$BACKUPDIR/oxibackup/$(hostname)" -type d | while read LINE;
+			do
+			        if [ $(find "$LINE" -type f | wc -l) == 0 ];
+			        then
+			                rmdir "$LINE"
+			        fi
+			done
+		fi
+
+		SIZEAFTER=$(du -sh $BACKUPDIR/oxibackup/$(hostname))
+
+		if [ $COUNT -gt 0 ]; then
+			notifyadmin "$(hostname) backup cleanup" "-- $(hostname) backup cleanup --\n\nfiles cleaned:\t$COUNT\nsize before:\t$SIZEBEFORE\nsize after:\t$SIZEAFTER"
+		fi
+	else
 	nofityadmin "backup cleanup FAIL" "please install fdupes!"
-    fi
-    
-    umountbackup
+	fi
+	
+	umountbackup
 }
 
--- jobs/backup-cleanup.sh
+++ jobs/backup-cleanup.sh
@@ -2,4 +2,5 @@
 . /etc/oxiscripts/backup.sh
 
 #This job searches for duplicated files and removes them.
+# add a number to backupcleanup to remove all backupfiles older than X Days
 backupcleanup
