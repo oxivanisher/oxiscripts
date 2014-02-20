179adce9edfa763830713eb3a2d3922987afdebd
Marc Urben <aegnor@mittelerde.ch>
2014-02-20 15:41:03 +0100

    backupcleanup can now remove "older than" files

--- jobs/backup-rsync.sh
+++ jobs/backup-rsync.sh
@@ -8,5 +8,8 @@ rsyncbackup /mnt/backup rsync://backup@your.host.com/rsync-dir "--bwlimit=1000"
 
 #the 3rd option is for passing options trough to rsync
 
+#if you use this feature to backup your files from /mnt/backup to somewhere else,
+#check the option to "remove older files than" in backup-cleanup.sh
+
 #For no password:
 RSYNCPASSWORD=""
