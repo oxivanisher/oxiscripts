2a0ac5e4291feae08c3e9ee7ea8e5f8ee0835bc3
Marc Urben <aegnor@mittelerde.ch>
2014-08-24 21:34:13 +0200

    update check now ignoring kept back upgrades (reduces the spam via email a lot)

--- debian/updatecheck.sh
+++ debian/updatecheck.sh
@@ -3,7 +3,7 @@
 
 /usr/bin/apt-get update >/dev/null
 
-if [ "$(/usr/bin/apt-get -d -s -q upgrade | grep "upgraded,")" != "0 upgraded, 0 newly installed, 0 to remove and 0 not upgraded." ];
+if [ "a$(/usr/bin/apt-get -d -s -q upgrade | grep 'upgraded,' | grep -v '0 upgraded, 0 newly installed, 0 to remove and')" != "a" ];
 then
 	ox-base-notifyadmin "APT Update Status on $(hostname)" "$(/usr/bin/apt-get upgrade -q -d -s)"
 fi
